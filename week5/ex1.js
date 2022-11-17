62b83fcc841f25ccd2958c04b6fb4f3d934e8f46
seanitchaharn <116155772+seanitchaharn@users.noreply.github.com>
2022-11-17 09:21:33 +0700

    Create ex1.js

---
+++ week5/ex1.js
@@ -0,0 +1,13 @@
+let items ={
+    "A4paper":120,
+    "Ruler":25,
+    "FlashDrive":130
+}
+//Mr. Lee
+console.log("Mr. Lee",items.A4paper*5 +items.Ruler*2)
+
+//Mr. Yue
+console.log("Mr. Yue",items.FlashDrive*2 +items.A4paper*3)
+
+//Ms.Sea
+console.log("Ms.Sea",items.FlashDrive*3 +items.A4paper*3+items.Ruler*2)
