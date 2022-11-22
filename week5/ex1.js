d3fdce057535e6050cd0a3274423e660b8725fa9
seanitchaharn <64309010032@tnk.ac.th>
2022-11-22 10:36:16 +0700

    Update ex1.js

--- week5/ex1.js
+++ week5/ex1.js
@@ -1,13 +1,29 @@
-let items ={
-    "A4paper":120,
-    "Ruler":25,
-    "FlashDrive":130
+let order =[{
+    "Customer":"Mr.Lee",
+    "ItemCrder" : ["A4 paper","Ruler"],
+    "Qty":[5,2],
+    "Price":[120,25]   
+},
+{
+    "Customer":"Mr.Yue",
+    "ItemCrder" : ["Flash Drive","A4 paper"],
+    "Qty":[2,3],
+    "Price":[130,120]   
+},
+{
+    "Customer":"Sea",
+    "ItemCrder" : ["Flash Drive","A4 paper","Ruler"],
+    "Qty":[10,20,30],
+    "Price":[130,120,50]   
+},
+];
+
+for (let i = 0; i < order.length; i++) {
+
+let sum = 0;
+for (let j = 0; j < order[i].ItemCrder.length; j++) {
+    sum += order[i].Qty[j] *order[i].Price[j]
 }
-//Mr. Lee
-console.log("Mr. Lee",items.A4paper*5 +items.Ruler*2)
+console.log(order[i].Customer, sum)
 
-//Mr. Yue
-console.log("Mr. Yue",items.FlashDrive*2 +items.A4paper*3)
-
-//Ms.Sea
-console.log("Ms.Sea",items.FlashDrive*3 +items.A4paper*3+items.Ruler*2)
+}
